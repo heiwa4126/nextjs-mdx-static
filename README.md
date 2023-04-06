fcc9202dc1ccad4e0b5430dde57d8257c6145fe3
heiwa4126 <heiwa4126@gmail.com>
2023-04-06 18:09:52 +0900

    update

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+```bash
+pnpm create next-app@latest nextjs-mdx --ts --use-pnpm --example app-dir-mdx
+```
+
 ## Getting Started
 
 First, run the development server:
---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import Link from "next/link";
+import Content from "./content.mdx";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "How about this?",
+};
+
+export default function About() {
+  return (
+    <main>
+      <Content />
+      <Link href="/">← back to home</Link>
+    </main>
+  );
+}
