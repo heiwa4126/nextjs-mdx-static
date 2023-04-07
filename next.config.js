a5b21a22ec7fdb53895f7f9a34a2c1cc9da8a453
heiwa4126 <heiwa4126@gmail.com>
2023-04-07 14:56:24 +0900

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  base: process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "./",
+  basePath: process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "./",
   experimental: {
     appDir: true,
     mdxRs: true,
   },
 };
-console.log("*****");
-console.log(process.env);
-console.log(nextConfig);
-console.log("*****");
+// console.log("*****");
+// console.log(process.env);
+// console.log(nextConfig);
+// console.log("*****");
 
 const withMDX = require("@next/mdx")();
 module.exports = withMDX(nextConfig);
